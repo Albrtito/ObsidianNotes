c996ccd80b0a066a548f58c3c5db690b4c95e071
tito-Mac <alb.pascau@gmail.com>
2024-10-11 23:38:34 +0200

    Autosave: 2024-10-11 23:38:34
    Affected files:
    99 - Meta/2. Scripts/test_yaml.py

--- 99 - Meta/2. Scripts/test_yaml.py
+++ 99 - Meta/2. Scripts/test_yaml.py
@@ -7,5 +7,5 @@ carpeta_atomic = "../../02 - Atomic"
 archivo = "20240429 - 111944 - Theorem - Euclid's proof of infinite prime numbers.md"
 with open(os.path.join(carpeta_atomic,archivo), "r",encoding="utf-8") as f:
     contenido = f.read()
-    yaml_match = re.match(r"^---\n(.*?)\n---", contenido, re.DOTALL)
+    yaml_match = re.search(r"^---\s*(.*?)\s*---", contenido, re.DOTALL)
     print(yaml_match)
